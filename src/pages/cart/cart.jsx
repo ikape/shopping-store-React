6f7582e451a0f7d18ad1722d874f321a52c90fc9
ikape <ikapeisaac3@gmail.com>
2023-04-21 01:00:46 +0100

    few chages

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,40 +1,46 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
-import { ShopContext } from '../../context/shop-context';
-import { CartItem } from './cart-item';
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
 import "./cart.css";
-
-import {useNavigate} from 'react-router-dom'
-
 export const Cart = () => {
-    const { cartItems, getTotalCartAmount } = useContext(ShopContext);
-    const totalAmount = getTotalCartAmount()
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    
   return (
-      <div className='cart'>
-          <div>
-              <h1>Your cart Items</h1>
-          </div>
-
-          <div className='cartItems'>
-              {PRODUCTS.map((product) => {
-                  if (cartItems[product.id] !== 0) {
-                      return <CartItem data={product} />
-                  }
-              })}
-          </div>
-          {totalAmount > 0 ? (
-              <div className='checkout'>
-                  <p>subtotal: ${totalAmount}</p>
-                  <button onClick={() => navigate("/")}>Continue Shopping</button>
-                  <button>Checkout</button>
-              </div>
-          ) : (
-            <h1> Your Cart is Empty</h1>      
-          )}
+    <div className='cart'>
+      <div>
+        <h1>Your Cart Items</h1>
       </div>
-  )
-}
+      <div className='cart'>
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div className='checkout'>
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
+    </div>
+  );
+};
